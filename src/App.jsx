9e7c50561ff0e76227fd66e5816c5d6fd9588b7a
Mohammed Salim <50304249+HazyAutumnRain@users.noreply.github.com>
2023-07-23 12:51:24 +0200

    Basic Alpha
    - Missing a lot of features
    - Added a working code editor
    - Added a working preview window
    - Code preview still buggy (Needs fixing)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Editor } from '@monaco-editor/react';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import remarkGfm from 'remark-gfm';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import { Box } from '@chakra-ui/layout';
+import rehypeHighlight from 'rehype-highlight/lib';
+import "highlight.js/styles/github.css";
+import 'katex/dist/katex.min.css'
+
 
 function App() {
+
+  const [ md, setMd ] = useState('')
+
+  function handle_editor_change(value) {
+    setMd(value);
+  }
+  
   return (
     <>
+    <Box display='flex' justifyContent={'space-between'}>
+      <Editor height='65vw' width='50vw' defaultLanguage='markdown' theme='vs-dark' onChange={handle_editor_change}/>
+      <Box backgroundColor='#1e1e1e' w='50.1vw' color='white'>
+        <ReactMarkdown remarkPlugins={[remarkGfm, remarkMath]} rehypePlugins={[rehypeKatex, rehypeHighlight]} className='markdown-body' >
+          {md}
+        </ReactMarkdown>
+      </Box>
+    </Box>
     </>
   )
 }
