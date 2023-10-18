1ff465d8ebcbf4bf3a82c8b52217ae0358ea5fc4
Mohammed Salim <50304249+HazyAutumnRain@users.noreply.github.com>
2023-10-18 12:55:24 +0400

    Initalize Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-import { useState } from 'react';
-import { Box, Heading, Icon } from '@chakra-ui/react';
-import { GiThunderBlade } from 'react-icons/gi'
-import { BsTwitter, BsGithub, BsYoutube } from 'react-icons/bs'
-import './App.css'
+import { Button } from '@mui/material'
+import './App.scss'
 
 function App() {
-
-  const [ load, setLoad ] = useState(false);
-  const [ md, setMd ] = useState('')
-
-  function handle_editor_change(value) {
-    setMd(value);
-  }
   
   return (
-    <>
-    	<Box>
-    		<Box display='flex' justifyContent='space-around'>
-    			<Icon as={GiThunderBlade} w={8} h={8} margin={2} />
-    			<Box display='flex' w='24' justifyContent='space-around'>
-    				<Icon as={BsTwitter} w={4} h={4} />
-    				<Icon as={BsGithub} w={4} h={4}/>
-    				<Icon as={BsYoutube} w={4} h={4}/>
-    			</Box>
-    		</Box>
-    	</Box>
-    </>
-  )
+		<>
+			<Button variant={'contained'}>Hello World!</Button>
+		</>
+	)
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,5 @@ import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
+  <App />
 )
