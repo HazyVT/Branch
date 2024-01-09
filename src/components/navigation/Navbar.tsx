5bd5cdd646c0f400e473f7315456219e099b5774
Mohammed Salim <renablehots@gmail.com>
2024-01-09 23:12:01 +0400

    Update Navbar.tsx

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -9,18 +9,19 @@ import { useState } from "react";
 export default function Navbar(props: {user: User | null}) {
 
   const { colorMode, toggleColorMode } = useColorMode();
-  const [ image, setImage ] = useState(props.user?.getData().image)
+  const [ image ] = useState(props.user?.getData().image)
 
 
   return (
     <Box w='100vw' h='1vh' display='flex' alignItems='center' justifyContent='space-between' padding={12} boxShadow={'0px 3px 3px rgba(0,0,0,0.1)'}>
       <Link to='/'><Text fontSize={24} fontWeight={500}>Branch</Text></Link>
-      <Box display='flex' w='10vw' justifyContent='space-around'>
+      <Box display='flex' w='14vw' justifyContent='space-around'>
         <Icon cursor={"pointer"} as={colorMode === 'light' ? CiDark : CiSun} onClick={toggleColorMode} w={6} h={6} />
         <Navicon icon={PiGithubLogo} />
         <Navicon icon={PiTwitterLogo} />
         <Navicon icon={PiYoutubeLogo} />
       </Box>
+
       <Link to={props.user ? '/account' : '/login'}><Avatar size='sm' src={image} bgColor='red.300' /></Link>
     </Box>
   )
