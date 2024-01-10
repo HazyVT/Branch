5c5485cb8ffdc310413945bba31bfef8ff0dc72a
Mohammed Salim <renablehots@gmail.com>
2024-01-10 12:03:11 +0400

    Profile loading state added
    - Added a loading state to when you switch profile pictures on the account page.

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,8 +1,9 @@
-import { Avatar, Box, Heading, Icon, Input, Text } from "@chakra-ui/react";
+import { Avatar, Box, Heading, Icon, Input, Spinner, Text } from "@chakra-ui/react";
 import { FaCamera } from "react-icons/fa";
 import User from "../models/User";
 import { ChangeEvent, MutableRefObject, useRef, useState } from "react";
 import { channel, supabase } from "../models/Client";
+import { BeatLoader } from "react-spinners";
 
 export default function Account(props: {user: User | null}) {
 
@@ -14,19 +15,19 @@ export default function Account(props: {user: User | null}) {
 
   // States
   const [ showAvatarEdit, setShowAvatarEdit ] = useState(false);
+  const [ loading, setLoading ] = useState(false);
   const [ image, setImage ] = useState(user?.getData().image);
 
   const deleteOldPhoto = async () => {
     const {error} = await supabase.storage.from('avatars').remove(['avatars/public/'+user?.getData().id+'.png']);
-    console.error(error);
   }
 
   const setNewImage = async (event: ChangeEvent<HTMLInputElement>) => {
     // Change image
     const file = event.target.files;
-    console.log(file);
     if (file != null && file.length > 0) {
       // get signed link
+      setLoading(true);
       deleteOldPhoto()
       const img = file[0];
       const { data, error } = await supabase.storage.from('avatars').upload('avatars/public/'+user?.getData().id+'.png', img, {
@@ -51,6 +52,7 @@ export default function Account(props: {user: User | null}) {
               event: 'image',
               payload: { image: url}
             })
+            setLoading(false);
           }
         }
       } else {
@@ -63,13 +65,16 @@ export default function Account(props: {user: User | null}) {
   return (
     <Box display='flex' flexDir='column' alignItems='center'>
       <Box bgColor='red.300' w='100vw' h='30vh' display='flex' justifyContent='center' pos='relative'>
-        <Icon as={FaCamera} pos='absolute' top='27vh' color='white' zIndex={3} w={12} h={12} 
+        <Icon display={loading ? 'none' : 'block'} as={FaCamera} pos='absolute' top='27vh' color='white' zIndex={3} w={12} h={12} 
           opacity={showAvatarEdit ? '30%' : '0'} 
           onMouseEnter={() => {setShowAvatarEdit(true)}} 
           onMouseLeave={() => {setShowAvatarEdit(false)}} 
           cursor={"pointer"} 
           onClick={() => {imgref.current.click()}}
         />
+        <Box display={loading ? 'block' : 'none'} pos='absolute' top='30vh' zIndex={3}> 
+          <BeatLoader size={8} />
+        </Box>
         <Avatar pos='absolute' src={image} size='xl' top='25vh' outline='8px solid white' bgColor='red.300' zIndex={2}/>
         <Input ref={imgref} type='file' display='none' accept=".jpg" onChange={setNewImage}/>
       </Box>
